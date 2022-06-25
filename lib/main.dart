42c5531d2a4b0ef09d6f17694e785cc5206d7caa
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-06-25 18:37:27 +0200

    bring in the 3rd party package

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,14 +14,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
     return MaterialApp(
       title: 'Sportsball',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter you man'),
+          title: const Text('⚽️ Sportsball ⚽️'),
         ),
-        body: const Center(
-          child: Text('Hello Worldies'),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
