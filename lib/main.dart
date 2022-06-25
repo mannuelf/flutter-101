c8f66c38112b9d4bf5f3c32d6d47007f6b888d88
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-06-25 19:20:59 +0200

    generating a list of words

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:ui';
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
@@ -21,9 +23,24 @@ class _RandomWordsState extends State<RandomWords> {
   final _biggerFont = const TextStyle(fontSize: 18);
 
   final wordPair = WordPair.random();
+
   @override
   Widget build(BuildContext context) {
-    return Text(wordPair.asPascalCase);
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return ListTile(
+          title: Text(_suggestions[index].asPascalCase),
+        );
+      },
+    );
   }
 }
 
@@ -33,10 +50,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sportsball',
+      title: 'RandomWordGenerator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('⚽️ Sportsball ⚽️'),
+          title: const Text('RandomWordGenerator'),
         ),
         body: const Center(
           child: RandomWords(),
